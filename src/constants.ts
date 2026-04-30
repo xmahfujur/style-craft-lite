79bd255deccd06701a5577025a204521321f6a51
Mahfujur <xmahfujur@gmail.com>
2026-04-30 19:50:28 +0600

    new

---
+++ src/constants.ts
@@ -0,0 +1,10 @@
+import { Product } from './types';
+
+export const PRODUCTS: Product[] = [];
+
+export const SHIPPING_RATES = {
+  inside_dhaka: 70,
+  outside_dhaka: 110,
+};
+
+export const SIZES = ['S', 'M', 'L', 'XL', 'XXL'] as const;
